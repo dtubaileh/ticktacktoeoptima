f43179148daa094f38b20eb97c185fa8f993fe22
dtubaileh <dtubaileh@asaltech.com>
2022-09-09 06:48:47 +0300

    finishing game

--- ticktackoeoptima/App.js
+++ ticktackoeoptima/App.js
@@ -9,56 +9,114 @@
 import React, { useEffect, useState } from 'react';
 import type { Node } from 'react';
 import {
+  Alert,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
 
 const App: () => Node = () => {
-  const [ticktack, setTicktak] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]); //[0, 0, 0, 0, 0, 0, 0, 0, 0]
+  const [ticktack, setTicktak] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
   const [winner, setWinner] = useState('Tie');
   const userX = 'X';
   const userO = 'O';
 
-  useEffect(() => { });
-
   const move = (value, index) => {
+    // as there is no winner you can continue with the game
     if (winner === 'Tie') {
       ticktack[index] = value;
       setTicktak([...ticktack]);
-      // Win Rows
-      if (ticktack[0] === ticktack[1] && ticktack[1] === ticktack[2]) {
+
+      // Win Diagonals
+      if (
+        ticktack[0] !== 0 &&
+        ticktack[0] === ticktack[4] &&
+        ticktack[4] === ticktack[8]
+      ) {
         setWinner(ticktack[0]);
-      } else if (ticktack[3] === ticktack[4] && ticktack[4] === ticktack[5]) {
+      } else if (
+        ticktack[2] !== 0 &&
+        ticktack[2] === ticktack[4] &&
+        ticktack[4] === ticktack[6]
+      ) {
+        setWinner(ticktack[1]);
+      }
+      // Win Rows
+      if (
+        ticktack[0] !== 0 &&
+        ticktack[0] === ticktack[1] &&
+        ticktack[1] === ticktack[2]
+      ) {
+        setWinner(ticktack[0]);
+      } else if (
+        ticktack[3] !== 0 &&
+        ticktack[3] === ticktack[4] &&
+        ticktack[4] === ticktack[5]
+      ) {
         setWinner(ticktack[3]);
-      } else if (ticktack[6] === ticktack[7] && ticktack[7] === ticktack[8]) {
+      } else if (
+        ticktack[6] !== 0 &&
+        ticktack[6] === ticktack[7] &&
+        ticktack[7] === ticktack[8]
+      ) {
         setWinner(ticktack[6]);
       }
 
       // Win Columns
-      else if (ticktack[0] === ticktack[3] && ticktack[3] === ticktack[6]) {
+      else if (
+        ticktack[0] !== 0 &&
+        ticktack[0] === ticktack[3] &&
+        ticktack[3] === ticktack[6]
+      ) {
         setWinner(ticktack[0]);
-      } else if (ticktack[1] === ticktack[4] && ticktack[4] === ticktack[7]) {
+      } else if (
+        ticktack[1] !== 0 &&
+        ticktack[1] === ticktack[4] &&
+        ticktack[4] === ticktack[7]
+      ) {
         setWinner(ticktack[1]);
-      } else if (ticktack[2] === ticktack[5] && ticktack[5] === ticktack[8]) {
+      } else if (
+        ticktack[2] !== 0 &&
+        ticktack[2] === ticktack[5] &&
+        ticktack[5] === ticktack[8]
+      ) {
         setWinner(ticktack[2]);
-      }
-
-      // Win Diagonals
-      else if (ticktack[0] === ticktack[4] && ticktack[4] === ticktack[8]) {
-        setWinner(ticktack[0]);
-      } else if (ticktack[2] === ticktack[4] && ticktack[4] === ticktack[6]) {
-        setWinner(ticktack[1]);
       }
     }
   };
 
-  
+  const checkValidInput = (inputText, index) => {
+    if (inputText === userX || inputText === userO) {
+      move(inputText, index);
+    } else {
+      Alert.alert('Invalid Input', 'please inter either X or O');
+    }
+  };
+
+  const matrixSquareInput = (itemValue, index) => {
+    if (itemValue === 0) {
+      return (
+        <TextInput
+          style={style.textStyle}
+          maxLength={1}
+          onChangeText={selectedValue => checkValidInput(selectedValue, index)}
+          placeholder="0"
+        />
+      );
+    } else {
+      return (
+        <View>
+          <Text style={style.textStyle}>{itemValue}</Text>
+        </View>
+      );
+    }
+  };
   const matrixViewBuilder = () => {
     const threePartIndex = Math.ceil(ticktack.length / 3);
     const updatedArray = [...ticktack];
@@ -70,29 +128,17 @@ const App: () => Node = () => {
       <>
         <View style={style.rowView}>
           {firstPart.map((item, index) => {
-            return (
-              <TouchableHighlight onPress={() => move(userX, index)}>
-                <Text style={style.textStyle}>{item}</Text>
-              </TouchableHighlight>
-            );
+            return matrixSquareInput(item, index);
           })}
         </View>
         <View style={style.rowView}>
           {secondPart.map((item, index) => {
-            return (
-              <TouchableHighlight onPress={() => move(userX, index + 3)}>
-                <Text style={style.textStyle}>{item}</Text>
-              </TouchableHighlight>
-            );
+            return matrixSquareInput(item, index + 3);
           })}
         </View>
         <View style={style.rowView}>
           {thirdPart.map((item, index) => {
-            return (
-              <TouchableHighlight onPress={() => move(userX, index + 6)}>
-                <Text style={style.textStyle}>{item}</Text>
-              </TouchableHighlight>
-            );
+            return matrixSquareInput(item, index + 6);
           })}
         </View>
       </>
@@ -102,7 +148,7 @@ const App: () => Node = () => {
   return (
     <SafeAreaView style={style.container}>
       <View style={style.columnView}>{matrixViewBuilder()}</View>
-      <Text style={style.winnerText}>{`Winner is: ${winner}`}</Text>
+      <Text style={style.textStyle}>{`Winner is: ${winner}`}</Text>
     </SafeAreaView>
   );
 };
@@ -116,6 +162,5 @@ const style = StyleSheet.create({
     flex: 1,
   },
   textStyle: { margin: 15, fontSize: 37 },
-  winnerText: { marginTop: 8 },
 });
 export default App;
